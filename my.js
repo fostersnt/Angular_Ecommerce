c0f9a2a35306a4797fcfef50d56a4c453d895c43
= <=>
2023-07-29 07:03:08 +0000

    Update

---
+++ my.js
@@ -0,0 +1,7 @@
+function lg(point) {
+    console.log(point.y + ' is ' + point.x);
+}
+lg({
+    x: 2,
+    y: 'Your age'
+});
---
+++ my.ts
@@ -0,0 +1,13 @@
+interface Point{
+    x: number,
+    y: string
+}
+
+function lg(point: Point) {
+    console.log(point.y + ' is ' + point.x);
+}
+
+lg({
+    x: 2,
+    y: 'Your age'
+});
